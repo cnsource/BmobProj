20b110eef115db5409662335b8ec398f41ad0238
creater <2274850295@qq.com>
2019-01-07 23:14:42 +0800

    2.0

--- app/src/main/java/com/bmobproj/demo/bmobproject/MainActivity.java
+++ app/src/main/java/com/bmobproj/demo/bmobproject/MainActivity.java
@@ -25,9 +25,8 @@ private   List<String> permissionsList=new ArrayList<>();
         minstence=this;
         setContentView(R.layout.activity_main);
         Bmob.initialize(this, "6efee115cb0d6e46cedd5a556ae2b9a0");
-        //需要申请的权限列表
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //判断版本号是否大于23
-            if ((checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) //判断该权限是否获得授权
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED))
                 permissionsList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
 
             if ((checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED))
@@ -36,8 +35,7 @@ private   List<String> permissionsList=new ArrayList<>();
             if (permissionsList.size() == 0) {
 
             } else {
-                requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
-                        1); //进行权限申请
+                requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),1);
             }
         }
         if (BmobUser.isLogin()){
@@ -48,7 +46,6 @@ private   List<String> permissionsList=new ArrayList<>();
             finish();
         }
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
