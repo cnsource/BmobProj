93eef010c3c023144bc40cbd4d711de893d53b52
creater <2274850295@qq.com>
2019-01-07 15:13:42 +0800

    基本布局

--- app/src/main/java/com/bmobproj/demo/bmobproject/RegisteActivity.java
+++ app/src/main/java/com/bmobproj/demo/bmobproject/RegisteActivity.java
@@ -3,12 +3,12 @@ package com.bmobproj.demo.bmobproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registe);
 
     }
 }
